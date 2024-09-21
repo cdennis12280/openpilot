299135b08e9b3ed87540e8ca4d2906da2c679788
Calvin Park <calvinspark@gmail.com>
2024-09-20 21:07:12 -0700

    Squashed commit of tranlocquy/siennahybrid
    https://github.com/tranlocquy/FrogPilot/commit/dbe50d9fedf8d211d32fa18938402780087cf530

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -341,7 +341,7 @@ class CarState(CarStateBase):
     if CP.flags & ToyotaFlags.ALT_GAS_MSG.value:
       messages.append(("GAS_PEDAL", 42))
 
-    if CP.carFingerprint not in [CAR.TOYOTA_MIRAI, CAR.TOYOTA_RAV4_PRIME]:
+    if CP.carFingerprint not in [CAR.TOYOTA_MIRAI, CAR.TOYOTA_RAV4_PRIME, CAR.TOYOTA_SIENNA_HYBRID]:
       messages.append(("ENGINE_RPM", 42))
 
     if CP.carFingerprint in UNSUPPORTED_DSU_CAR:
